55885f15df57db1c9f771e239fb323d287aa3c87
kjcodeacct <kjcodeacct@gmail.com>
2020-07-06 21:58:25 -0500

    added menu , setup rest of app, TODO finish plugin and ui tools

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:unicorndial/unicorndial.dart';
+import 'dart:io';
 import 'dart:developer' as developer;
 
 void main() => runApp(MyApp());
@@ -47,10 +49,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   // TODO - break file handling out into mixin or other state class
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   String loadedImagePath = 'assets/default.png';
+  File loadedImage;
+
   String workingImagePath;
   String _path;
   Map<String, String> _paths;
@@ -84,8 +87,11 @@ class _MyHomePageState extends State<MyHomePage> {
     if (!mounted) return;
     setState(() {
       _loadingPath = false;
-      loadedImagePath =
-          _path != null ? _path.split('/').last : _paths != null ? _paths.keys.toString() : '...';
+
+      if (_path != null) {
+        loadedImagePath = _path;
+      }
+
       print("loading image $loadedImagePath");
     });
   }
@@ -111,12 +117,38 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // This method is rerun every time setState is called, for instance as done on _openFileExplorer
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    loadedImage = new File(loadedImagePath);
+
+    var menuButton = List<UnicornButton>();
+
+    menuButton.add(UnicornButton(
+        hasLabel: true,
+        labelText: "Save",
+        currentButton: FloatingActionButton(
+          heroTag: "save",
+          backgroundColor: Colors.greenAccent,
+          mini: true,
+          child: Icon(Icons.save_alt),
+          onPressed: () => _openFileExplorer(),
+        )));
+
+    menuButton.add(UnicornButton(
+        hasLabel: true,
+        labelText: "Open",
+        currentButton: FloatingActionButton(
+          heroTag: "open",
+          backgroundColor: Colors.grey,
+          mini: true,
+          child: Icon(Icons.folder_open),
+          onPressed: () => _openFileExplorer(),
+        )));
+
+    // rebuild ui
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -126,38 +158,57 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              loadedImagePath,
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Text(
+                  'File open $loadedImagePath',
+                ),
+              ),
+              Flexible(
+                  flex: 12,
+                  child: Container(
+                    margin: const EdgeInsets.all(30.0),
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Theme.of(context).dividerColor,
+                          // width: 5.0,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                    child: Image.file(loadedImage),
+                  )),
+              Flexible(
+                flex: 8,
+                child: Container(color: Colors.orange),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _openFileExplorer(),
-        tooltip: 'Open Image',
-        child: Icon(Icons.folder_open),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: UnicornDialer(
+        orientation: UnicornOrientation.VERTICAL,
+        parentButton: Icon(Icons.menu),
+        childButtons: menuButton,
+      ),
     );
   }
 }
